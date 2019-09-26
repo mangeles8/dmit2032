1d1e0ae73b2c17e179ae34d69c9081edfc6746ae
Francis Angeles <mangeles8@nait.ca>
2019-09-26 09:53:05 -0600

    last update

---
+++ jabber-v-01/footer.php
@@ -0,0 +1,11 @@
+
+    <footer>
+        <ul>
+            <li>this</li>
+            <li>is the</li>
+            <li>page footer</li>
+        </ul>
+    </footer>
+   <?php wp_footer() ?>    <!--this has to be included. one of wordpress' prerequisites. it is very important. !without this, your PLUGINS wont work! -->
+    </body>
+    </html>
---
+++ jabber-v-01/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <?php 
+        wp_head() // this has to be included. one of wordpress' prerequisites. it is very important. !without this, your PLUGINS wont work!
+    ?>
+</head>
+
+
+<body>
+    <nav>
+        <h2>Site Navigation</h2>
+    </nav>
+    <header>
+    <h1>Jabber Header</h1>
+    </header>
